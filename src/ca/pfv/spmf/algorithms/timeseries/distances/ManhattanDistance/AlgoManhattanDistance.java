9e80190a6f3a4995095f3bbb67fcc9a63019cb89
kostissss <kostevage@gmail.com>
2025-05-02 09:56:47 +0300

    wrapping Euclidian Distance and ManhattanDistance in order to be used with timeseriesObjects

---
+++ src/ca/pfv/spmf/algorithms/timeseries/distances/ManhattanDistance/AlgoManhattanDistance.java
@@ -0,0 +1,51 @@
+package ca.pfv.spmf.algorithms.timeseries.distances.ManhattanDistance;
+
+import ca.pfv.spmf.algorithms.clustering.distanceFunctions.DistanceManathan;
+import ca.pfv.spmf.algorithms.timeseries.TimeSeries;
+import ca.pfv.spmf.tools.MemoryLogger;
+
+import java.io.IOException;
+
+public class AlgoManhattanDistance {
+
+    /** the time the algorithm started */
+    long startTimestamp = 0;
+    /** the time the algorithm terminated */
+    long endTimestamp = 0;
+
+    DistanceManathan _distanceFunction;
+    public AlgoManhattanDistance() {
+        _distanceFunction = new DistanceManathan();
+    }
+
+
+    /**
+     * Calculate the Manhattan distance between two time series.
+     * @param timeSeries1 the first time series
+     * @param timeSeries2 the second time series
+     * @return the Manhattan distance
+     */
+
+
+    public double runAlgorithm(TimeSeries timeSeries1, TimeSeries timeSeries2) throws IOException {
+
+        startTimestamp = System.currentTimeMillis();
+        if (timeSeries1.size() != timeSeries2.size()) {
+            throw new IllegalArgumentException("The two time series must have the same size.");
+        }
+
+
+        double result = _distanceFunction.calculateDistance(timeSeries1, timeSeries2);
+        return  result;
+    }
+
+
+    public void printStats() {
+        System.out.println("=============  Transform to Exponential Smoothing v2.21- STATS =============");
+        System.out.println(" Total time ~ " + (endTimestamp - startTimestamp) + " ms");
+        System.out.println(" Max Memory ~ " + MemoryLogger.getInstance().getMaxMemory() + " MB");
+        System.out.println("===================================================");
+    }
+
+
+}
---
+++ src/ca/pfv/spmf/algorithms/timeseries/distances/ManhattanDistance/MainTestManhattanDistance.java
@@ -0,0 +1,38 @@
+package ca.pfv.spmf.algorithms.timeseries.distances.ManhattanDistance;
+
+import ca.pfv.spmf.algorithms.timeseries.TimeSeries;
+import ca.pfv.spmf.algorithms.timeseries.exponentialsmoothing.AlgoExponentialSmoothing;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+
+public class MainTestManhattanDistance {
+
+    public static void main(String [] arg) throws IOException {
+
+        // the smoothing constant (a double representing a percentage between 0 and 1)
+        double alpha = 1;
+
+        // Create a time series
+        double [] dataPoints1 = new double[]{1.0, 4.5, 6.0, 4.0, 3.0, 4.0, 5.0, 4.0, 3.0, 2.0};;
+        double [] dataPoints2 = new double[]{3.0, 4.5, 5.0, 6.0, 3.0, 4.0, 8.0, 4.0, 3.0, 2.0};;
+        TimeSeries timeSeries1 = new TimeSeries(dataPoints1, "SERIES1");
+        TimeSeries timeSeries2 = new TimeSeries(dataPoints2, "SERIES2");
+
+        // Applying the  algorithm
+        AlgoManhattanDistance algorithm = new AlgoManhattanDistance();
+        double distance = algorithm.runAlgorithm(timeSeries1, timeSeries2);
+        algorithm.printStats();
+
+        // Print the moving average
+        System.out.println(" Manhattan Distance: ");
+        System.out.println(distance);
+
+    }
+
+    public static String fileToPath(String filename) throws UnsupportedEncodingException {
+        URL url = MainTestManhattanDistance.class.getResource(filename);
+        return java.net.URLDecoder.decode(url.getPath(),"UTF-8");
+    }
+}
